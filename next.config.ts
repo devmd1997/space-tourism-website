b5a4360b9217d1d43b8e2e80e31269bef0c10426
devmd1997 <devmd1997@gmail.com>
2025-11-04 23:59:15 -0500

    optimizing images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    qualities: [25, 50, 75, 100],
+    deviceSizes: [375, 640, 1440]
+  }
 };
 
 export default nextConfig;
--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Image from "next/image";
+import Image, { getImageProps } from "next/image";
 import { useEffect, useState } from "react";
 import { useScreenSizeQuery } from "@/lib/features/useScreenSizeQuery";
 
@@ -17,43 +17,57 @@ interface BackgroundLayoutProps {
   children: React.ReactNode;
 }
 
+const common = { sizes: "100vw", fill: true, quality: 100, alt: "Background" };
+
 export default function BackgroundLayout({ children }: BackgroundLayoutProps) {
-  const currentScreen = useScreenSizeQuery();
   const pathname = usePathname();
-  const [backgroundImageSrc, setBackgroundImageSrc] = useState("");
-  const [isMounted, setIsMounted] = useState(false);
+
+  const {
+    props: { srcSet: desktop },
+  } = getImageProps({
+    ...common,
+    src: `${backgroundMap[pathname] || backgroundMap["/"]}-desktop.jpg`,
+  });
+
+  const {
+    props: { srcSet: tablet },
+  } = getImageProps({
+    ...common,
+    src: `${backgroundMap[pathname] || backgroundMap["/"]}-tablet.jpg`,
+  });
+
+  const {
+    props: { srcSet: mobile, ...rest },
+  } = getImageProps({
+    ...common,
+    src: `${backgroundMap[pathname] || backgroundMap["/"]}-mobile.jpg`,
+  });
 
   // This effect runs once on the client after initial render to confirm mounting.
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  // useEffect(() => {
+  //   setIsMounted(true);
+  // }, []);
 
-  // This effect runs when the path or screen size changes, but only after mounting.
-  useEffect(() => {
-    // Don't run on the server, before hydration, or if the screen size isn't determined yet.
-    if (!isMounted || !currentScreen) return;
+  // // This effect runs when the path or screen size changes, but only after mounting.
+  // useEffect(() => {
+  //   // Don't run on the server, before hydration, or if the screen size isn't determined yet.
+  //   if (!isMounted || !currentScreen) return;
 
-    const newImage = `${
-      backgroundMap[pathname] || backgroundMap["/"]
-    }-${currentScreen}.jpg`; // Fallback to home background
-    setBackgroundImageSrc(newImage);
-    console.log(newImage);
-  }, [pathname, currentScreen, isMounted]);
+  //   const newImage = `${
+  //     backgroundMap[pathname] || backgroundMap["/"]
+  //   }-${currentScreen}.jpg`; // Fallback to home background
+  //   setBackgroundImageSrc(newImage);
+  //   console.log(newImage);
+  // }, [pathname, currentScreen, isMounted]);
 
   return (
     <div className="background-layout-container">
-      {backgroundImageSrc && (
-        <Image
-          key={backgroundImageSrc}
-          src={backgroundImageSrc}
-          alt="Space tourism background"
-          fill
-          sizes="100vw"
-          priority
-          quality={100}
-          className="background-layout-image"
-        />
-      )}
+      <picture>
+        <source media="(min-width: 1440px) " srcSet={desktop} />
+        <source media="(min-width: 768px)" srcSet={tablet} />
+        <source media="(max-width: 767px)" srcSet={mobile} />
+        <img {...rest} className="background-layout-image" />
+      </picture>
       <div className="background-layout-content">{children}</div>
     </div>
   );
